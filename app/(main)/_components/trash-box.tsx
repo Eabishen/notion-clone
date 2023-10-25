34ba4cd36f10d6b69e92b619e690e6b20ed8c0f2
Eabishen <ebi4lbe@gmail.com>
2023-10-25 18:59:04 +0530

    sidebar trash folder designing

--- app/(main)/_components/trash-box.tsx
+++ app/(main)/_components/trash-box.tsx
@@ -7,8 +7,9 @@ import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { toast } from "sonner";
 import { Spinner } from "@/components/spinner";
-import { Search } from "lucide-react";
+import { Search, Trash, Undo } from "lucide-react";
 import { Input } from "@/components/ui/input";
+import { ConfirmModal } from "@/components/modals/confirm-modal";
 
 const TrashBox = () => {
   const router = useRouter();
@@ -54,20 +55,58 @@ const TrashBox = () => {
     }
   };
 
-  if(documents === undefined){
+  if (documents === undefined) {
     return (
       <div className="flex items-center justify-center p-4 h-full">
         <Spinner size="lg" />
       </div>
-    )
+    );
   }
 
-  return <div className="text-sm">
-    <div className="flex items-center gap-x-1 p-2">
-      <Search className="h-4 w-4" />
-      <Input />
+  return (
+    <div className="text-sm">
+      <div className="flex items-center gap-x-1 p-2">
+        <Search className="h-4 w-4" />
+        <Input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="h-7 px-2 focus-visible:ring-transparent bg-secondary"
+          placeholder="Search notes..."
+        />
+      </div>
+      <div className="mt-2 px-1 pb-1">
+        <p className="hidden last:block text-xs text-center text-muted-foreground pb-2">
+          No documents found.
+        </p>
+        {filteredDocuments?.map((document) => (
+          <div
+            key={document._id}
+            role="button"
+            onClick={() => onClick(document._id)}
+            className="text-sm rounded-sm w-full hover:bg-primary/5 flex items-center justify-between text-primary"
+          >
+            <span className="truncate pl-2">{document.title}</span>
+            <div className="flex items-center">
+              <div
+                onClick={(e) => onRestore(e, document._id)}
+                className="p-2 rounded-sm hover:bg-neutral-200"
+              >
+                <Undo className="w-4 h-4 text-muted-foreground" />
+              </div>
+              <ConfirmModal onConfirm={() => onRemove(document._id)}>
+                <div
+                  role="button"
+                  className="p-2 rounded-sm hover:bg-neutral-200"
+                >
+                  <Trash className="h-4 w-4 dark:text-red-800 text-red-400" />
+                </div>
+              </ConfirmModal>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default TrashBox;
